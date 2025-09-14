52c3d12dc05a55eb25ce253e9fe8af8f593b56a7
Sandeep Kamble <sandeep@Sandeeps-MacBook-Pro.local>
2025-09-14 16:33:28 +0530

    deleted capability-service

--- services/capability-registry/app/db/mongodb.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from ..config import settings
-_client = None
-async def get_db():
-    global _client
-    if _client is None:
-        _client = AsyncIOMotorClient(settings.mongo_uri)
-    return _client[settings.mongo_db]
